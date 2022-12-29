54b607d90f2c1ae196f74d54754bfe362596a7e1
CarlosMarioToro <111100510+CarlosMarioToro@users.noreply.github.com>
2022-12-28 23:07:54 -0500

    Clase 8: Location y hash navigation

---
+++ src/navigation.js
@@ -0,0 +1,12 @@
+function navigator() {
+    if (location.hash.startsWith('#trends')) {
+        console.log('TRENDS');
+    } else if (location.hash.startsWith('#search=')) {
+        console.log('Search!!');
+    } else if (location.hash.startsWith('#movie=')) {
+        console.log('Movie!!');
+    } else if (location.hash.startsWith('#categories=')) {
+        console.log('Categoria!!');
+    }
+    location.hash
+}
